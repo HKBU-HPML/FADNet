4180c3cf0c1fb75793d8797378f714628bd5d746
wuqing <wuqing@galasports.net>
2018-01-15 23:16:09 +0800

    add res-block for deconv. (not tested)

--- layers_package/layers.py
+++ layers_package/layers.py
@@ -34,3 +34,32 @@ class ResBlock(nn.Module):
         out = self.relu(out)
         return out
 
+class DeconvResBlock(nn.Module):
+    def __init__(self, n_in, n_out, stride = 2):
+        super(DeconvResBlock, self).__init__()
+        self.deconv1 = nn.ConvTranspose2d(n_in, n_out, kernel_size = 4, stride = stride, padding = 1)
+        self.bn1 = nn.BatchNorm2d(n_out)
+        self.relu = nn.ReLU(inplace = True)
+        self.deconv2 = nn.ConvTranspose2d(n_out, n_out, kernel_size = 3, padding = 1)
+        self.bn2 = nn.BatchNorm2d(n_out)
+
+        if stride != 1 or n_out != n_in:
+            self.shortcut = nn.Sequential(
+                nn.ConvTranspose2d(n_in, n_out, kernel_size = 1, stride = stride),
+                nn.BatchNorm2d(n_out))
+        else:
+            self.shortcut = None
+
+    def forward(self, x):
+        residual = x
+        if self.shortcut is not None:
+            residual = self.shortcut(x)
+        out = self.deconv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.deconv2(out)
+        out = self.bn2(out)
+        
+        out += residual
+        out = self.relu(out)
+        return out
