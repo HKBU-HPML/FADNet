798ce9277a4823937e99e33d6661032a22d71c10
wuqing <wuqing@galasports.net>
2018-01-15 10:52:51 +0800

    dispnetC with resblock runs successfully.

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-python main.py --cuda --outf ./models-resnet --lr 0.01 --logFile train-resnet.log --showFreq 1 --devices 0,1,2,3
+# dispnet with resnet
+# python main.py --cuda --outf ./models-resnet --lr 0.01 --logFile train-resnet.log --showFreq 1 --devices 0,1,2,3
+
+# dispnetC with resnet
+python main.py --cuda --outf ./models-dispC-resnet --lr 0.01 --logFile train-dispC-resnet.log --showFreq 1 --devices 0,1,2,3
