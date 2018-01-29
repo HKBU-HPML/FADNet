a3bc58f804fb019f79ed84b1f7e7eac2bb7aad80
shyhuai <shaohuaishi@gmail.com>
2018-01-29 11:51:19 +0800

    detect csr

--- detect.sh
+++ detect.sh
@@ -1,1 +1,2 @@
-python detecter.py --model model_best.pth --rp ./detect_result --filepath /home/datasets/imagenet/
+#python detecter.py --model ./cleandata-models-dispC-resnet/dispS_epoch_8.pth --rp ./detect_result_cd --filepath /home/datasets/imagenet/
+python detecter.py --model ./dispCSRes-model_best.pth --rp ./detect_result_csr --filepath /home/datasets/imagenet/ --filelist CLEAN_FlyingThings3D_release_TEST.list
