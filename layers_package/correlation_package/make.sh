d1e5862972af40a46d2d6a3dab18859d030614a9
wuqing <wuqing@galasports.net>
2018-01-29 12:40:25 +0800

    fix layers_package for different GPUs.

--- layers_package/correlation_package/make.sh
+++ layers_package/correlation_package/make.sh
@@ -8,7 +8,12 @@ echo "Compiling correlation kernels by nvcc..."
 rm correlation_cuda_kernel.o
 rm -r ../_ext
 
-nvcc -c -o correlation_cuda_kernel.o correlation_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_35
+nvcc -c -o correlation_cuda_kernel.o correlation_cuda_kernel.cu -x cu -Xcompiler -fPIC \
+			   -gencode arch=compute_35,code=sm_35 \
+			   -gencode arch=compute_37,code=sm_37 \
+			   -gencode arch=compute_52,code=sm_52 \
+			   -gencode arch=compute_60,code=sm_60 
+
 
 cd ../
 python build.py
