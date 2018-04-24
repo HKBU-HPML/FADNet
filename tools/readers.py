d3ebee11158a04f7bb26a5de2f36fac5917fc63e
shyhuai <shaohuaishi@gmail.com>
2018-04-24 19:02:38 +0800

    add preprocessing script

---
+++ tools/readers.py
@@ -0,0 +1,21 @@
+import numpy as np
+import PyEXR as exr
+import sys
+
+def load_exr(filename, single_depth=False):
+    exrimg = exr.PyEXRImage(filename, single_depth)
+    #exrimg = exr.PyEXRImage(filename)
+    print('height, width: ', exrimg.height, exrimg.width)
+    disp_arr = np.array(exrimg, copy = False)
+    print('disp shape: ', disp_arr.shape)
+    try:
+        rgba = np.reshape(disp_arr, (exrimg.height, exrimg.width, 4)) 
+    except Exception as e:
+        print('Exception: ', filename, e)
+        rgba = np.reshape(disp_arr, (exrimg.height, exrimg.width)) 
+    rgb = rgba[:,:,0:3]
+    rgb = np.flip(rgb, 0)
+    print('shape: ', rgb.shape)
+    return rgb 
+
+
