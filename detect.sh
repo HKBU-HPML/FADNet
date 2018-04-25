313c18e1abb63a33d008cf71d5f0917cd0b7574b
shyhuai <shaohuaishi@gmail.com>
2018-04-25 14:35:12 +0800

    add training with finetue for girl dataset

--- detect.sh
+++ detect.sh
@@ -1,11 +1,12 @@
 #python detecter.py --model ./cleandata-models-dispC-resnet/dispS_epoch_8.pth --rp ./detect_result_cd --filepath /home/datasets/imagenet/
-#python detecter.py --model ./dispCSRes-model_best.pth --rp ./detect_result_csr --filepath /home/datasets/imagenet/ --filelist CLEAN_FlyingThings3D_release_TEST.list
+#python detecter.py --model ./cleandata-dispCSRes-model_best.pth --rp ./detect_result_csr --filepath /home/datasets/imagenet/ --filelist ./lists/CLEAN_FlyingThings3D_release_TEST.list
+
 # python detecter.py --model model_best.pth --rp ./detect_result --filepath /home/datasets/imagenet/
 
 # detect with dispCSRes
 # python detecter.py --model ./cleandata-models-dispCSRes/model_best.pth --rp ./detect_result_crop --filepath /home/datasets/imagenet
 
-# python detecter.py --model ./cc-cleandata-dispCSRes-model_best.pth --rp ./cc_detect_result --filepath /home/datasets/imagenet
+#python detecter.py --model ./cc-cleandata-dispCSRes-model_best.pth --rp ./cc_detect_result --filepath /home/datasets/imagenet
 #python detecter.py --model ./models/cleandata-dispCSRes-model_best.pth --rp ./flying_detect_cleandata --filepath /home/datasets/imagenet --filelist FlyingThings3D_release_TEST.list
 #python detecter.py --model ./cc-models-dispC-resnet-cleandata-mix/model_best.pth --rp ./flying_detect_occlusion --filepath /home/datasets/imagenet --filelist RB_FlyingThings3D_release_TEST.list
 #python detecter.py --model ./cleandata-models-dispCSR/model_best.pth --rp ./flying_detect_cleandata --filepath /home/datasets/imagenet --filelist FlyingThings3D_release_TEST.list
@@ -14,7 +15,7 @@
 # python detecter.py --model ./models/cc-dispCSRes-model_best.pth --rp ./cc-detect_result_l2norm --filepath /home/datasets/imagenet --devices 1,2 --batchSize 2 
 
 # FlyingThings
-# python detecter.py --model ./models/cc-dispCSRes-model_best.pth --rp ./cc_detect_result_l2norm --filepath data 
+#python detecter.py --model ./models/cc-dispCSRes-model_best.pth --rp ./cc_detect_result_l2norm --filepath data 
 # python detecter.py --model ./cleandata-models-dispC-resnet-relu-ft-rb/model_best.pth --rp ./flying_detect_result --filepath data --filelist RB_FlyingThings3D_release_TEST.list
 
 # real camera
@@ -26,4 +27,4 @@
 # python detecter.py --model ./models/cc-dispCSRes-model_best.pth --rp ./cc-detect_result_l2norm --filepath /home/datasets/imagenet
 
 # girl data
-python detecter.py --model ./cleandata-models-dispCSR-girl/model_best.pth --rp ./girl_detect_cleandata --filepath /home/datasets/imagenet/dispnet/virtual --filelist girl_TRAIN.list
+python detecter.py --model ./girl-models-dispCSRes-finetune/model_best.pth --rp ./girl_detect_cleandata --filepath /home/datasets/imagenet/dispnet/virtual/girl --filelist ./lists/girl.list
