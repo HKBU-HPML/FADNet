88e82709d4617ed94b26f2340c46534a9d4e3e71
wuqing <wuqing@galasports.net>
2018-05-20 19:12:33 +0800

    training new neural network.

--- run_v2.sh
+++ run_v2.sh
@@ -1,7 +1,7 @@
 # dispnetCSR with all 3x3 kernels + clean data
 # python main.py --cuda --outf ./models/cleandata-models-dispCSR-ks --lr 0.001 --logFile cleandata-train-dispCSR-ks.log --showFreq 1 --devices 0,1,2,3 --trainlist ./lists/SHAOHUAI_CLEAN_FlyingThings3D_release_TRAIN.list --vallist ./lists/SHAOHUAI_CLEAN_FlyingThings3D_release_TEST.list --endEpoch 100
-# python main.py --cuda --outf ./models/cleandata-models-dispCSR-ks --lr 1e-5 --logFile cleandata-train-dispCSR-ks-cont2.log --showFreq 1 --devices 0,1,2,3 --trainlist ./lists/SHAOHUAI_CLEAN_FlyingThings3D_release_TRAIN.list --vallist ./lists/SHAOHUAI_CLEAN_FlyingThings3D_release_TEST.list --model ./models/cleandata-models-dispCSR-ks/model_best.pth --endEpoch 50
+python main.py --cuda --outf ./models/cleandata-models-dispCSR-ks --lr 2.5e-5 --logFile cleandata-train-dispCSR-ks-cont4.log --showFreq 1 --devices 0,1 --trainlist ./lists/SHAOHUAI_CLEAN_FlyingThings3D_release_TRAIN.list --vallist ./lists/SHAOHUAI_CLEAN_FlyingThings3D_release_TEST.list --model ./models/cleandata-models-dispCSR-ks/model_best.pth --endEpoch 30 --batchSize 4
 
 # dispnetCSR best model + KITTI
-python main.py --cuda --outf ./models/kitti-models-dispCSR --lr 0.001 --logFile kitti-train-dispCSR.log --showFreq 1 --devices 0,1,2,3 --trainlist ./lists/KITTI_TRAIN.list --vallist ./lists/KITTI_TEST.list --endEpoch 100 --model ./models/cleandata-models-dispCSRes-exp/model_best.pth --endEpoch 100 --devices 0,1
+# python main.py --cuda --outf ./models/kitti-models-dispCSR --lr 0.001 --logFile kitti-train-dispCSR.log --showFreq 1 --devices 0,1,2,3 --trainlist ./lists/KITTI_TRAIN.list --vallist ./lists/KITTI_TEST.list --model ./models/cleandata-models-dispCSRes-exp/model_best.pth --endEpoch 300 --devices 0,1
 
