896f5a29c8120714bbbfc5dd22fc2d3bb288466f
shyhuai <shaohuaishi@gmail.com>
2018-06-06 21:25:57 +0800

    add detect using new domain adaptation model

---
+++ detect-v3.sh
@@ -0,0 +1,2 @@
+#python detecter_v3.py --model ./models/girl-domain-transfer-models-dispCSRes/model_best.pth --rp ./detect_result/domain_adaptation --filepath /home/datasets/imagenet/dispnet/virtual --filelist ./lists/girl09.list
+python detecter_v3.py --model ./models/girl-domain-transfer-models-dispCSRes/model_best.pth --rp ./detect_result/domain_adaptation --filepath /home/datasets/imagenet/dispnet/virtual --filelist ./lists/real_release.list
