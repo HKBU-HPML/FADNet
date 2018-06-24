38f138db0c7fc345ab60502116145f6e69299f2d
wuqing <wuqing@galasports.net>
2018-06-24 11:30:25 +0800

    reise list

--- tools/check_imagesize.py
+++ tools/check_imagesize.py
@@ -3,26 +3,34 @@ import numpy as np
 import os
 import sys
 from skimage import io
+from dataset import load_pfm
 
-orignal_list = '../lists/girl_with_ir2'
+orignal_list = './lists/girl_with_ir2'
 root_dir = '/data2/virtual3'
-target_list = '../lists/exclude_1536.list'
-
+target_list = './lists/exclude_1536.list'
 tf = open(target_list, 'w')
+
 with open(orignal_list+'.list', 'r') as f:
     for line in f.readlines():
-        r = line.split(' ')
+        r = line.strip('\n').split(' ')
         if len(r) < 4:
             print('error: ', line)
             print(r)
             continue
-        leftir = r[0]
-        rightir = r[4]
-        path = os.path.join(root_dir, leftir)
-        img_left = io.imread(path)
-        if img_left.shape[0] != 2048:
-            print(path)
-        else:
+        error = False
+        for im in r:
+            path = os.path.join(root_dir, im)
+            img = None
+            if path.find('.png') > 0:
+                img = io.imread(path)
+            else:
+                img, scale = load_pfm(path)
+            #print('name: ', im, ', shape: ', img.shape)
+            if img.shape[0] != 2048 or img.shape[1] != 2048:
+                print('error img: ', img.shape, line, im)
+                error = True
+                break
+        if not error:
             tf.write(line)
 tf.close()
 
