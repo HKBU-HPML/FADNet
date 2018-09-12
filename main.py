15f93f86fcc0f20b49f503e2f2e51f932b651927
wuqing <wuqing@galasports.net>
2018-09-13 01:15:27 +0800

    minor revision.

--- main.py
+++ main.py
@@ -29,10 +29,6 @@ parser.add_argument('--unsupervised', type=bool, help='if open the function of d
 parser.add_argument('--unsuper_alpha', type=float, help='weight of unsupervised learning', default=1.0)
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=16)
 parser.add_argument('--input_channel', type=int, help='with or without ir', default=3)
-<<<<<<< HEAD
-parser.add_argument('--workers', type=int, help='number of data loading workers', default=16)
-=======
->>>>>>> 23ed753fde5ff2fb34bd675c3a3f722f75acc917
 parser.add_argument('--batchSize', type=int, default=8, help='input batch size')
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum for sgd, alpha parameter for adam. default=0.9')
