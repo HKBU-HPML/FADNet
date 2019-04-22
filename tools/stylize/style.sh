a0324a0daa59677af4304be08016ad629ff65dc7
blackjack201312 <blackjack201303@gmail.com>
2019-04-22 13:21:42 +0000

    remove stylize.

--- tools/stylize/style.sh
+++
@@ -1,4 +0,0 @@
-python neural_style/neural_style.py eval --content-image ./content_images/real/left/img00055.bmp --model ./flying_models/test.model --output-image ./stylized_images/real/left/img00055.bmp --cuda 1
-python neural_style/neural_style.py eval --content-image ./content_images/real/left/img00000.bmp --model ./flying_models/test.model --output-image ./stylized_images/real/left/img00000.bmp --cuda 1
-python neural_style/neural_style.py eval --content-image ./content_images/real/right/img00055.bmp --model ./flying_models/test.model --output-image ./stylized_images/real/right/img00055.bmp --cuda 1
-python neural_style/neural_style.py eval --content-image ./content_images/real/right/img00000.bmp --model ./flying_models/test.model --output-image ./stylized_images/real/right/img00000.bmp --cuda 1
--- tools/stylize/virtual_style.sh
+++
@@ -1,1 +0,0 @@
-python neural_style/neural_style.py eval --content-list ./girl_style.list --model ./saved_models/candy.pth --cuda 1
