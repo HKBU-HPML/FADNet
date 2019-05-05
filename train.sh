94d16c6afec51c2154a883aa14afbc1a19b89407
blackjack201312 <blackjack201303@gmail.com>
2019-05-05 15:38:30 +0000

    finish dispnetcres training(imagent version.)

--- train.sh
+++ train.sh
@@ -1,18 +1,18 @@
 net=dispnetcres
-loss=loss_configs/dispnetcres_flying_1.json
-lr=0.001
-outf_model=models/dispCSRes-1
-logf=logs/dispCSRes-1.log
+loss=loss_configs/dispnetcres_flying.json
+lr=1e-4
+outf_model=models/dispCSRes
+logf=logs/dispCSRes.log
 devices=0,1,2,3
 trainlist=lists/SHAOHUAI_CLEAN_FlyingThings3D_release_TRAIN.list
 vallist=lists/SHAOHUAI_CLEAN_FlyingThings3D_release_TEST.list
-startR=0
-startE=5
-endE=20
+startR=1
+startE=0
+endE=50
 batchSize=32
 #model=none
-model=models/dispCSRes-1/dispnetcres_0_4.pth
-#model=models/dispnet-corr1d-best.pth
+#model=models/dispCSRes-1/dispnetcres_0_19.pth
+model=models/dispCSRes/model_best.pth
 python main.py --cuda --net $net --loss $loss --lr $lr \
                --outf $outf_model --logFile $logf \
                --devices $devices --batch_size $batchSize \
