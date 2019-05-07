ff0da84002453267965f126e0b8be53f6768cd76
blackjack201312 <blackjack201303@gmail.com>
2019-05-07 03:32:40 +0000

    add multi-scale loss for KITTI training.

--- finetune.sh
+++ finetune.sh
@@ -1,16 +1,17 @@
 
-#python kitti_finetune.py --maxdisp 192 \
-#                   --model dispnetcres \
-#                   --devices 0,1,2 \
-#                   --datatype 2015 \
-#                   --datapath data/KITTI_release/training/ \
-#                   --epochs 900 \
-#                   --savemodel ./trained/ \
-#                   --loadmodel ./models/model_best.pth \
+python kitti_finetune.py --maxdisp 192 \
+                   --model dispnetcres \
+                   --devices 0,1,2,3 \
+                   --datatype 2015 \
+                   --datapath data/KITTI_release/training/ \
+                   --epochs 900 \
+                   --savemodel ./trained/dispnet-imagenet/ \
+                   --loadmodel ./models/dispnet-imagenet-best.pth \
+                   --loss loss_configs/dispnetcres_flying.json \
 
-python kitti_submission.py --maxdisp 192 \
-                     --model dispnetcres \
-                     --KITTI 2015 \
-                     --datapath /data/KITTI_release/testing/ \
-                     --savepath submit_results/dispnetcres_KITTI2015/ \
-                     --loadmodel trained/best.tar \
+#python kitti_submission.py --maxdisp 192 \
+#                     --model dispnetcres \
+#                     --KITTI 2015 \
+#                     --datapath /data/KITTI_release/testing/ \
+#                     --savepath submit_results/dispnetcres_imagenet_finetune300_KITTI2015/ \
+#                     --loadmodel trained/dispnet-imagenet/best.tar \
--- losses/multiscaleloss.py
+++ losses/multiscaleloss.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import math
 import numpy as np
+import torch.nn.functional as F
 
 def EPE(input_flow, target_flow):
     # print(input_flow.size(), target_flow.size())
@@ -16,15 +17,17 @@ def EPE(input_flow, target_flow):
     #hist = np.histogram(EPE_map.data.cpu().numpy(), bins=10)
     #print(hist)
     # print(input_flow.sum())
+    return F.smooth_l1_loss(input_flow, target_flow, size_average=True)
 
-    EPE_map = torch.norm(target_flow - input_flow + 1e-16, 2, 1)
-    return EPE_map.mean()
+    #EPE_map = torch.norm(target_flow - input_flow + 1e-16, 2, 1)
+    #return EPE_map.mean()
 
 class MultiScaleLoss(nn.Module):
 
-    def __init__(self, scales, downscale, weights=None, loss='L1'):
+    def __init__(self, scales, downscale, weights=None, loss='L1', mask=False):
         super(MultiScaleLoss, self).__init__()
         self.downscale = downscale
+        self.mask = mask
         self.weights = torch.Tensor(scales).fill_(1).cuda() if weights is None else torch.Tensor(weights).cuda()
         assert(len(self.weights) == scales)
 
@@ -50,15 +53,20 @@ class MultiScaleLoss(nn.Module):
                 target_ = self.multiScales[i](target)
                 ## consider the scale effects
                 #target_ = self.multiScales[i](target) / (2**i)
+                #print('target shape: ', target_.shape, ' input shape: ', input_.shape)
+                if self.mask:
+                    mask = target_ > 0
+                    input_ = input_[mask]
+                    target_ = target_[mask]
                 EPE_ = EPE(input_, target_)
                 out += self.weights[i] * EPE_
         else:
             out = self.loss(input, self.multiScales[0](target))
         return out
 
-def multiscaleloss(scales=5, downscale=4, weights=None, loss='L1', sparse=False):
+def multiscaleloss(scales=5, downscale=4, weights=None, loss='L1', sparse=False, mask=False):
     if weights is None:
         weights = (0.005, 0.01, 0.02, 0.08, 0.32)
     if scales == 1 and type(weights) is not tuple:
         weights = (weights, )
-    return MultiScaleLoss(scales, downscale, weights, loss)
+    return MultiScaleLoss(scales, downscale, weights, loss, mask)
