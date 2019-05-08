0c8ef7a41846546d3402ceb8f39c02c18742ceb0
blackjack201312 <blackjack201303@gmail.com>
2019-05-08 04:49:16 +0000

    limit the disparity below 192.

--- train.sh
+++ train.sh
@@ -5,14 +5,16 @@ outf_model=models/dispCSRes
 logf=logs/dispCSRes.log
 devices=0,1,2,3
 trainlist=lists/SHAOHUAI_CLEAN_FlyingThings3D_release_TRAIN.list
-vallist=lists/SHAOHUAI_CLEAN_FlyingThings3D_release_TEST.list
+#vallist=lists/SHAOHUAI_CLEAN_FlyingThings3D_release_TEST.list
+vallist=lists/FlyingThings3D_release_TEST.list
 startR=1
 startE=0
 endE=0
 batchSize=32
 #model=none
 #model=models/dispCSRes-1/dispnetcres_0_19.pth
-model=models/dispCSRes/model_best.pth
+#model=models/dispCSRes/model_best.pth
+model=models/model_best.pth
 python main.py --cuda --net $net --loss $loss --lr $lr \
                --outf $outf_model --logFile $logf \
                --devices $devices --batch_size $batchSize \
