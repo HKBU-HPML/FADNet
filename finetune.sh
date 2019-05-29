abf03d64985d560a6e3feeadb87e46e502aa0003
blackjack2015 <qiangwang@comp.hkbu.edu.hk>
2019-05-28 19:00:50 -0700

    local change

--- finetune.sh
+++ finetune.sh
@@ -3,16 +3,15 @@ python kitti_finetune.py --maxdisp 192 \
                    --model dispnetcres \
                    --devices 0,1,2,3 \
                    --datatype 2015 \
-                   --datapath data/KITTI_release/training/ \
-                   --epochs 900 \
-                   --savemodel ./trained/dispnet-imagenet-argument/ \
-                   --loss loss_configs/dispnetcres_flying.json \
-                   --loadmodel ./trained/dispnet-imagenet-argument/best.tar 
-                   #--loadmodel ./models/dispCSRes/model_best.pth 
+                   --datapath /datasets/kitti/training/ \
+                   --epochs 2000 \
+                   --savemodel /datasets/sh_kittis/ShKittiTrained/dispnet-imagenet-argument/ \
+                   --loss loss_configs/kitti.json \
+                   --loadmodel  /datasets/sh_kittis/models/ShDispCSRes/model_best.pth
 
-python kitti_submission.py --maxdisp 192 \
-                     --model dispnetcres \
-                     --KITTI 2015 \
-                     --datapath /data/KITTI_release/testing/ \
-                     --savepath submit_results/dispnetcres_imagenet_argument-pad_KITTI2015/ \
-                     --loadmodel trained/dispnet-imagenet-argument/best.tar \
+#python kitti_submission.py --maxdisp 192 \
+#                     --model dispnetcres \
+#                     --KITTI 2015 \
+#                     --datapath /datasets/kitti/testing/ \
+#                     --savepath /datasets/sh_kittis/submit_results/dispnetcres_imagenet_argument-pad_KITTI2015/ \
+#                     --loadmodel /datasets/sh_kittis/ShKittiTrained/dispnet-imagenet-argument/best.tar \
