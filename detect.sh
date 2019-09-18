9ee9fdab2ac8b04c519c56c322f420c7063ce851
blackjack2015 <qiangwang@comp.hkbu.edu.hk>
2019-09-18 02:13:29 -0700

    update ablation study.

--- detect.sh
+++ detect.sh
@@ -1,6 +1,9 @@
-model=models/dispnet-corr1d-best.pth
-outf=detect_results/dispnetcres-flying-test
+#net=psmnet
+#model=models/psmnet/model_best.pth
+net=dispnetcres
+model=models/dispnetcres-sl/dispnetcres_2_8.pth
+outf=detect_results/psmnet-flying/
 filelist=lists/SHAOHUAI_CLEAN_FlyingThings3D_release_TEST.list
 filepath=data
 
-CUDA_VISIBLE_DEVICES=0 python detecter.py --model $model --rp $outf --filelist $filelist --filepath $filepath
+CUDA_VISIBLE_DEVICES=0,1 python detecter.py --model $model --rp $outf --filelist $filelist --filepath $filepath --devices 0,1 --net ${net}
