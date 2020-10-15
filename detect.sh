b58fe36df9b768d2b2e0ac03c581de99931ae7bf
shyhuai <shaohuaishi@gmail.com>
2020-10-15 10:29:22 +0800

    revise detection timing

--- detect.sh
+++ detect.sh
@@ -1,10 +1,11 @@
-net=psmnet
+net=mobilefadnet
 dataset=sceneflow
 
-model=models/psmnet.pth
+model=models/mobilefadnet/model_best.pth
 outf=detect_results/${net}-${dataset}/
 
-filelist=lists/FlyingThings3D_release_TEST.list
+#filelist=lists/FlyingThings3D_release_TEST.list
+filelist=lists/nano_fake.list
 filepath=data
 
 CUDA_VISIBLE_DEVICES=0 python detecter.py --model $model --rp $outf --filelist $filelist --filepath $filepath --devices 0 --net ${net} 
