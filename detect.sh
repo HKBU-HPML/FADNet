550caac4cc854337049080ea8164c349a5da1011
shyhuai <shaohuaishi@gmail.com>
2020-10-15 10:45:51 +0800

    revise detecter

--- detect.sh
+++ detect.sh
@@ -1,7 +1,7 @@
-net=psmnet
+net=mobilefadnet
 dataset=sceneflow
 
-model=models/psmnet.pth
+model=models/mobilefadnet/model_best.pth
 outf=detect_results/${net}-${dataset}/
 
 filelist=lists/FlyingThings3D_release_TEST.list
