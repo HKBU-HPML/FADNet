bb5d9ed75bc41a8365ed0a9b96e85c8cb632f083
shyhuai <shaohuaishi@gmail.com>
2020-10-15 12:37:37 +0800

    add tensorrt debug

--- detect.sh
+++ detect.sh
@@ -8,4 +8,5 @@ outf=detect_results/${net}-${dataset}/
 filelist=lists/nano_fake.list
 filepath=data
 
-CUDA_VISIBLE_DEVICES=0 python detecter.py --model $model --rp $outf --filelist $filelist --filepath $filepath --devices 0 --net ${net} 
+#CUDA_VISIBLE_DEVICES=0 python detecter.py --model $model --rp $outf --filelist $filelist --filepath $filepath --devices 0 --net ${net} 
+CUDA_VISIBLE_DEVICES=0 python detecter_rt.py --model $model --rp $outf --filelist $filelist --filepath $filepath --devices 0 --net ${net} 
