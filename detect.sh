7fed69bc0c5a4e2191cf06eb6400de0a58908e07
shyhuai <shaohuaishi@gmail.com>
2020-10-27 20:50:28 +0800

    add lightfadnet

--- detect.sh
+++ detect.sh
@@ -5,12 +5,12 @@ dataset=sceneflow
 model=models/${net}-sceneflow-v1/model_best.pth
 outf=detect_results/${net}-${dataset}-trt/
 #outf=detect_results/${net}-${dataset}/
+
 filelist=lists/nano_test.list
-
 #filelist=lists/FlyingThings3D_release_TEST.list
-filelist=lists/nano_fake.list
-#filepath=/datasets
-filepath=./data
+#filelist=lists/nano_fake.list
+filepath=/datasets
+#filepath=./data
 
-#CUDA_VISIBLE_DEVICES=0 python detecter.py --model $model --rp $outf --filelist $filelist --filepath $filepath --devices 0 --net ${net} 
-CUDA_VISIBLE_DEVICES=0 python detecter_rt.py --model $model --rp $outf --filelist $filelist --filepath $filepath --devices 0 --net ${net} 
+CUDA_VISIBLE_DEVICES=0 python detecter.py --model $model --rp $outf --filelist $filelist --filepath $filepath --devices 0 --net ${net} 
+#CUDA_VISIBLE_DEVICES=0 python detecter_rt.py --model $model --rp $outf --filelist $filelist --filepath $filepath --devices 0 --net ${net} 
