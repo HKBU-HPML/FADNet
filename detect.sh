be08e3bc4ca262d85f5a3dfc119019d28af3c8c7
shyhuai <shaohuaishi@gmail.com>
2020-10-27 08:59:34 -0500

    merge from master

--- detect.sh
+++ detect.sh
@@ -9,8 +9,8 @@ outf=detect_results/${net}-${dataset}-trt/
 filelist=lists/nano_test.list
 #filelist=lists/FlyingThings3D_release_TEST.list
 #filelist=lists/nano_fake.list
-filepath=/datasets
-#filepath=./data
+#filepath=/datasets
+filepath=./data
 
 CUDA_VISIBLE_DEVICES=0 python detecter.py --model $model --rp $outf --filelist $filelist --filepath $filepath --devices 0 --net ${net} 
 #CUDA_VISIBLE_DEVICES=0 python detecter_rt.py --model $model --rp $outf --filelist $filelist --filepath $filepath --devices 0 --net ${net} 
