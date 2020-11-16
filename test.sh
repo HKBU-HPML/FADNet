03c152e7c616d7cb3049ee8b3821b3796ced55d8
blackjack2015 <qiangwang@comp.hkbu.edu.hk>
2020-11-16 11:39:14 +0800

    finish baseline training.

--- test.sh
+++ test.sh
@@ -6,18 +6,19 @@ datapath=/datasets
 dataset=sceneflow
 trainlist=lists/SceneFlow.list
 vallist=lists/FlyingThings3D_release_TEST.list
+#vallist=lists/flying_short.list
 
 loss=loss_configs/test.json
 outf_model=models/test/
-logf=logs/${net}_test_on_${dataset}-v1.log
+logf=logs/${net}_test_on_${dataset}.log
 
 lr=1e-4
 devices=0,1,2,3
 startR=0
 startE=0
-batchSize=8
-#model=models/fadnet.pth
-model=models/mobilefadnet-sceneflow-v1/model_best.pth
+batchSize=32
+model="${model:-models/fadnet-sceneflow/model_best.pth}"
+#model=models/mobilefadnet-sceneflow-v3/model_best.pth
 python main.py --cuda --net $net --loss $loss --lr $lr \
                --outf $outf_model --logFile $logf \
                --devices $devices --batch_size $batchSize \
